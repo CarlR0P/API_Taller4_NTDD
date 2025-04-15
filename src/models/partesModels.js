7ac3add87a3adc4597cf09b3aea033b80fe09766
CarlR0P <davidrp123@hotmail.com>
2025-04-15 03:54:01 -0500

    Segundo Commit
    Se termina de implementar el CRUD para el modulo de partes. Se implementa modulo para registro e inicio de sesión. Se implementa protección con bcrypt y JWT.

--- src/models/partesModels.js
+++ src/models/partesModels.js
@@ -8,13 +8,41 @@ const partesSchema = mongoose.Schema({
         type: String,
         required: true,
     },
-    fabricante: {
+    marca: {
         type: String,
         required: true,
     },
     modelo: {
         type: String,
         requiered: true,
+    },
+    precio: {
+        type: Number,
+        requiered: true,
+    },
+    disponibilidad: {
+        type: Boolean,
+        requiered: true,
+    },
+    stock: {
+        type: Number,
+        requiered: true,
+    },
+    numeroSerie: {
+        type: String,
+        requiered: false,
+    },
+    fecha: {
+        type: Date,
+        requiered: false,
+    },
+    descripcion: {
+        type: String,
+        requiered: false,
+    },
+    especificaciones: {
+        type: String,
+        requiered: false,
     }
 });
 module.exports = mongoose.model("Partes", partesSchema);
--- src/routes/partesRoutes.js
+++ src/routes/partesRoutes.js
@@ -1,13 +1,60 @@
+const verifyToken = require('./validate_token');
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const partesSchema = require("../models/partesModels");
+
 //Nueva parte
-router.post("/partes", (req, res) => {
+router.post("/partes", verifyToken, (req, res) => {
     const parte = partesSchema(req.body);
     parte
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+
+//Consultar todos las partes
+router.get("/partes", (req, res) => {
+    partesSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Consultar una parte por su id
+router.get("/partes/:id", (req, res) => {
+    const { id } = req.params;
+    partesSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Modificar el nombre de un animal por su id
+router.put("/partes/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, tipo, fabricante, modelo } = req.body;
+    partesSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, tipo, fabricante, modelo }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Eliminar un animal por su id
+router.delete("/partes/:id", (req, res) => {
+    const { id } = req.params;
+    partesSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
 module.exports = router;
+
+
+
 
